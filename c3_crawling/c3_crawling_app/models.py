1b2ee1dbd6dec733cfaeaef8fbb27dc087985d3d
h0725j <guswldpqj1@naver.com>
2024-12-22 18:11:34 +0900

    ✨ [Feat] : 올리브영 랭킹 크롤링
    - 올리브영 랭킹 크롤링
    - 크롤링 후 ranking 테이블에 저장

--- c3_crawling/c3_crawling_app/models.py
+++ c3_crawling/c3_crawling_app/models.py
@@ -1,19 +1,17 @@
 from django.db import models
 
 class Ranking(models.Model):
-    id = models.AutoField(primary_key=True)
-    brand = models.CharField(max_length=100)
-    cosmetic_name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    oy_price = models.DecimalField(max_digits=10, decimal_places=2)
-    zz_price = models.DecimalField(max_digits=10, decimal_places=2)
-    cosmetic_url = models.URLField()
-    image_url = models.URLField()
+    brand = models.CharField(max_length=255)
+    cosmetic_name = models.CharField(max_length=255)
+    price = models.CharField(max_length=255, null=True, blank=True)
+    oy_price = models.CharField(max_length=255)
+    zz_price = models.CharField(max_length=255, null=True, blank=True)
+    cosmetic_url = models.TextField()  # URL이 길 수 있으므로 TextField 사용
+    image_url = models.TextField()  # URL이 길 수 있으므로 TextField 사용
 
     class Meta:
         db_table = 'ranking'
 
     def __str__(self):
-        return self.cosmetic_name
+        return f"{self.brand} - {self.cosmetic_name}"
 
-# Create your models here.
