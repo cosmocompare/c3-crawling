d64a70501ad0fb5fbcfc8e0a73d6947528fc3b90
h0725j <guswldpqj1@naver.com>
2024-12-22 13:46:56 +0900

    🗃 [Database] : 데이터베이스 관련 수정

--- c3_crawling/c3_crawling/settings.py
+++ c3_crawling/c3_crawling/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'c3_crawling.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'c3',
+        'USER': 'root',
+        'PASSWORD': '0725',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
